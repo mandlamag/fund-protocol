6b0fdcfd0b58701828703965feaf4a22b6e1e84e
Mandla <mandla.magagula@gmail.com>
2019-05-16 07:59:11 +0200

    Removed UI project

--- migrations/config/datafeed.js
+++ migrations/config/datafeed.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    navServiceUrl: 'json(http://b9d04835.ngrok.io/api/v1/portfolio).data',
+    navServiceUrl: 'json(http://bb12a75a.ngrok.io/api/v1/portfolio).data',
     dataFeedAddress: '0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475',
   },
   ropsten: {
