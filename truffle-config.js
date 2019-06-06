69236e6ee1f7ed9683057ddbc755866fec452115
Mandla <mandla.magagula@gmail.com>
2019-06-06 18:16:51 +0200

    Update contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,29 +1,32 @@
-module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*", // Match any network id
-      gas: 700000000,
+import HDWalletProvider from 'truffle-hdwallet-provider';
+require('dotenv').config();
+export const networks = {
+  development: {
+    host: "localhost",
+    port: 8545,
+    network_id: "*",
+    gas: 700000000,
+  },
+  test: {
+    host: "localhost",
+    port: 7545,
+    network_id: "*",
+    gas: 7000000,
+  },
+  ropsten: {
+    provider: () => {
+      return new HDWalletProvider(process.env.MNEMONIC, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY);
     },
-    test: {
-      host: "localhost",
-      port: 7545,
-      network_id: "*", // Match any network id
-      gas: 7000000,
-    },
-    ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: "3",
-      gas: 7000000,
-      gasPrice: 20000000000, // 20 GWei
-    },
-    mainnet: {
-      host: "localhost",
-      port: 8545,
-      network_id: "1",
-      gasPrice: 20000000000, // 20 GWei
-    }
+    network_id: 3,
+    gas: 5500000,
+    confirmations: 2,
+    timeoutBlocks: 400,
+    skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+  },
+  mainnet: {
+    host: "localhost",
+    port: 8545,
+    network_id: "1",
+    gasPrice: 20000000000,
   }
 };
